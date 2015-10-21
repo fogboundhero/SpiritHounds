acc4d5d504cade56ceddca30ef021d7c0774a9a4
guzman24 <guzmanjose24@gmail.com>
2015-10-20 18:44:03 -0700

    AI
    Simple AI using the NavMesAgent

---
+++ SpiritHounds/AI_Test1/Assets/Enemy2.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy2 : MonoBehaviour {
+
+	public float speed = 2.0f;
+	public Transform Target = null;
+	public float playerDistance;
+	public float rotationDamping;
+	
+	// Use this for initialization
+	void Start () {
+	
+
+
+	}
+	
+
+	void Update () {
+		//transform.Translate (Vector3.down * Time.deltaTime * speed);
+
+		playerDistance = Vector3.Distance(Target.position, transform.position);
+
+		if (playerDistance < 20.0f) {
+			
+			lookAtPlayer ();
+
+		}
+
+		if (playerDistance < 20.0f){
+
+			
+			chasePlayer();
+
+		}
+
+
+	}
+
+	void lookAtPlayer(){
+
+		Quaternion rotation = Quaternion.LookRotation (Target.position - transform.position);
+		transform.rotation = Quaternion.Slerp (transform.rotation, rotation, Time.deltaTime * rotationDamping);
+
+	}
+
+	void chasePlayer(){
+		//transform.GetComponent<NavMeshAgent> ().transform.Translate (Vector3.forward * speed * Time.deltaTime);
+		transform.GetComponent<NavMeshAgent> ().destination = Target.position;
+		//transform.Translate (Vector3.forward * speed * Time.deltaTime);
+
+//		Target = GameObject.Find ("Player").transform;
+//			
+//				if (Target != null) 
+//				{
+//						Vector3 dir = Target.position - transform.position;
+//		
+//						transform.position += (Target.position - transform.position).normalized 
+//						* speed * Time.deltaTime;
+//					}
+	}
+
+}
