1c9080c1322d5f9f3381c12cb16377cb923b9277
williamburgstaler1989 <williamburgstaler@gmail.com>
2016-03-23 19:09:16 -0700

    GDCDemo
    Build for gdc, includes, demonstration shown.

---
+++ SpiritHounds-master/SpiritHounds-master/Assets/Scripts/GDCDemoloader.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(AudioSource))]
+public class GDCDemoloader : MonoBehaviour {
+
+	public AudioClip houseClip;
+	public AudioClip barnClip;
+	public AudioClip endClip;
+
+	IEnumerator Start() {
+		AudioSource audio = GetComponent<AudioSource>();
+
+		audio.Play();
+		yield return new WaitForSeconds(audio.clip.length);
+		audio.clip = houseClip;
+		yield return new WaitForSeconds(5);
+		audio.Play();
+		yield return new WaitForSeconds(audio.clip.length);
+		audio.clip = barnClip;
+		yield return new WaitForSeconds(5);
+		audio.Play();
+		yield return new WaitForSeconds(audio.clip.length);
+		audio.clip = endClip;
+		yield return new WaitForSeconds(5);
+		audio.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.Backslash))
+		{
+			
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+		}
+	}
+}
